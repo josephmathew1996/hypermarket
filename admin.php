c4e7daa626b989fedafbfcf7ce8dc546a2e0d346
Joseph Chakkalakal Mathew <josephmathew1401@gmail.com>
2018-09-05 00:05:39 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,214 @@
+<?php
+ 
+   session_start();
+   include('session.php');
+   
+   $user=(Session::getInstance())->getSession();
+
+   if($user!="market_admin")
+   {
+    header('Location: http://localhost/hypermarket.com/login.php');
+   }
+   else
+   {
+
+     class products
+     {
+                  
+      public function __construct()
+
+      {
+   
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $database =  "market";
+
+    // Create MySQL connection
+    $this->conn = new mysqli($servername, $username, $password,$database);
+
+    // Check connection - if it fails, output will include the error message
+    if ($this->conn->connect_error) {
+        die('<p>Connection failed: <p>' . $this->conn->connect_error);
+    }
+   //echo '<p>Connected successfully</p>';
+      
+      //echo 'constructor called';
+    }
+     
+     public  function getProducts()
+     {
+       //global $len,$res;
+       $sql="SELECT name FROM products";
+       $result = $this->conn->query($sql);
+       
+       //var_dump($result->num_rows); 
+       
+       if($result->num_rows>0)
+       {
+         
+          $data =mysqli_fetch_all($result,MYSQLI_ASSOC);
+          return $data;
+          /*print_r($data);  
+           $i=0;
+          foreach($data as $x => $x_value) 
+          {
+              $d=$x_value;
+
+           foreach($d as $x1 => $x_value1)
+           {
+             
+             //echo $x_value1;
+             $res[$i]=$x_value1;
+             $i++;
+             
+           }
+           
+              //print_r($d) ;
+         }
+         
+        $len= sizeof($res);
+        echo $len;*/
+       } 
+       else
+       {
+
+        echo 'No data';
+       }
+        
+
+        
+     }
+    public function addimagedata($fileName,$productName)
+    {
+     // echo $fileName.'<br>'.$productName;
+     
+      $stmt=$this->conn->prepare("SELECT * FROM products WHERE name=?");
+      $stmt->bind_param('s',$productName);
+      $stmt->execute();
+      $result = $stmt->get_result();
+
+      //echo '<br>'.$result->num_rows;
+
+      //echo $result;
+      
+        if($result->num_rows===0)
+        {
+           echo 'no product found';
+           //header('Location: http://localhost/hypermarket.com/login.php');
+
+        }
+        else
+        {
+           echo 'product found<br>';
+           $row = $result->fetch_assoc();
+           $id=$row['id'];
+           //echo $id;
+           $imagePath="images/".$fileName;
+           //echo $imagePath;
+           //echo $row['name'];
+           //var_dump($row);
+           $stmt = $this->conn->prepare("UPDATE products SET image_path=? WHERE id=?");
+           $stmt->bind_param("si",$imagePath,$id);
+           $stmt->execute();
+          // echo $stmt->affected_rows.'<br>'.$imagePath;
+          
+                   if($stmt->affected_rows===0)
+                {
+                    die("error in uploading imagedata".$this->conn->connect_error);
+
+                }
+                else
+                {
+                    echo 'image data added successfully'; //change to html success message
+                }
+       
+        }
+   
+         $stmt->close();
+         $this->conn->close();
+     
+  }
+
+     public function logoutFun()
+    {
+        
+         session_destroy();
+         //echo $_SESSION['login_user1'];
+         header('Location: http://localhost/hypermarket.com/login.php');
+    }
+
+  }
+ 
+    $productObj = new products();
+  
+  
+    $res=$productObj->getProducts();
+     //var_dump($res);
+  
+  if(isset($_POST['logout']))
+    {
+
+       $productObj->logoutFun();
+         
+    }
+    if(isset($_POST['submit']))
+    {
+      if(isset($_FILES['image']['name']) && isset($_POST['product']))
+     {
+      $productObj->addimagedata($_FILES['image']['name'],$_POST['product']);
+     }
+
+    }
+    
+
+}
+
+?>
+
+<html>
+<head>
+    <title>Admin</title>
+</head>
+    <body>
+
+        <br>
+        <br>
+        <div style="text-align: center;">
+           <h1 >Add product image to databse</h1>
+           <form action="admin.php" method="post" enctype="multipart/form-data">
+           <input type="file" name="image">
+           <br>
+           <br>
+           <select name="product">
+            <option value="" selected>Select a product</option>
+            <?php
+              foreach ($res as $value) {
+            ?>
+            <option value="<?php echo $value['name']; ?>"> <?php echo $value['name']; ?> </option>
+            <?php
+          
+              }
+            ?>
+          </select>
+          <br><br>
+           <button type="submit" name="submit">Submit</button>
+           </form>        
+        </div>
+
+         <br>
+        <br>
+       
+
+      <div style="float: right;">
+          <form action="admin.php" method="post">
+              <button type="submit" name="logout">Logout</button>
+          </form>
+      </div> 
+    
+       
+    <script type="text/javascript">
+      
+    </script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php 
+    
+    class homeClass
+    {
+        public function __construct()
+
+    {
+
+      // In the variables section below, replace user and password with your own MySQL credentials as created on your server
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database =  "market";
+
+    // Create MySQL connection
+    $this->conn = new mysqli($servername, $username, $password,$database);
+
+    // Check connection - if it fails, output will include the error message
+    if ($this->conn->connect_error) {
+        die('<p>Connection failed: <p>' . $this->conn->connect_error);
+    }
+  //echo '<p>Connected successfully</p>';
+      
+    //  echo 'constructor called';
+    }
+      public function displayProducts()
+
+      {
+
+       $sql="SELECT * FROM products";
+       $result = $this->conn->query($sql);
+
+       if($result->num_rows>0)
+       {
+         
+          $row =mysqli_fetch_all($result,MYSQLI_ASSOC);
+          return $row;
+          
+          //print_r($row);
+           
+           //echo $row['name'].'<br>'.$row['price'].'<br>'.$row['quantity'].'<br>';
+      }
+      else
+       {
+
+        echo 'No data';
+       }
+        
+  }
+
+ }
+
+ $homeObj =new homeClass();
+ $res=$homeObj->displayProducts();
+?>
+
+<html>
+<head>
+    <title>Home</title>
+</head>
+    <body>
+        <br>
+        <br>
+        <h1 style="text-align: center;">All products</h1>
+        <?php
+          foreach ($res as $value) {
+        ?>
+        <div>     
+            <img style="width: 200px;height: 200px;" src="<?php echo $value['image_path'] ?>">
+            <h1><?php echo $value['name']?></h1>
+            <h2><?php echo $value['price']?></h2>
+            <h3><?php echo $value['quantity']?></h3>
+        </div>
+       <?php
+         }
+         ?>
+</body>
+</html>
